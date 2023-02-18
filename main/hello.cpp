2a6ddc41e0109b6aac8531e4d39b9334f4c8b0dc
Sharath K <79495971+sharathkrishnegowda@users.noreply.github.com>
2023-02-18 20:06:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "NAME : SHARATH SRN: PES2UG20CS552";
   return 0;
 }
